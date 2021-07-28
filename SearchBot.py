21d639d90f76f5483440d9f50eb4803ef61ad5db
YashPotdukhe <yashpotdukhe12@gmail.com>
2021-07-28 14:47:36 -0400

    Commit #1

---
+++ SearchBot.py
@@ -0,0 +1,43 @@
+import tweepy
+import time
+import requests
+import json
+
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+# Authenticate to Twitter
+
+
+api_key = "YxXvMP0WR67DsbCmyS7jtBhjE"
+api_secret = "GA5REhUgjF8YC4dtGHPcYY5ZDiIBkTK3M6mtQy6X3oSJVkWe06"
+access_token = "1419689610452537344-Ac98Jyr20pWtunx2Fcb8FC2slPTGlu"
+access_secret = "hhfhzjhaDgE1lE2bC7NJ9d8MMVbpCrC6zkNf8ktaPSb6v"
+
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token, access_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+hashtag = '#MotivationalQuote'
+
+tweetNum = 10
+
+tweets = tweepy.Cursor(api.search, hashtag).items(tweetNum)
+
+def searchBot():
+    for tweet in tweets:
+        try:
+            tweet.retweet()
+            print("Retweet Done!")
+            time.sleep(60*60*5)
+        except tweepy.TweepError as e:
+            print(e.reason)
+            time.sleep(60*60*5)
+
+searchBot()
---
+++ bot.py
@@ -0,0 +1,66 @@
+import tweepy
+import time
+import requests
+import json
+
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+# Authenticate to Twitter
+
+
+api_key = "YxXvMP0WR67DsbCmyS7jtBhjE"
+api_secret = "GA5REhUgjF8YC4dtGHPcYY5ZDiIBkTK3M6mtQy6X3oSJVkWe06"
+access_token = "1419689610452537344-Ac98Jyr20pWtunx2Fcb8FC2slPTGlu"
+access_secret = "hhfhzjhaDgE1lE2bC7NJ9d8MMVbpCrC6zkNf8ktaPSb6v"
+
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token, access_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+FILE_NAME = 'last_seen.txt'
+
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+
+def reply():
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
+    for tweet in reversed(tweets):
+        if '@TheMotivationP3' in tweet.full_text.lower():
+            print(str(tweet.id) + '-' + tweet.full_text)
+            api.update_status("@" + tweet.user.screen_name + " Keep working!", tweet.id)
+            api.create_favorite(tweet.id)
+            api.retweet(tweet.id)
+            store_last_seen(FILE_NAME, tweet.id)
+
+
+#while True:
+    #reply()
+    #time.sleep(15)
+
+
+
+# api.update_status("Hello World")
+
+response = requests.get('https://type.fit/api/quotes')
+
+# jprint(response.json())
+
+author = response.json()[0]['author']
+jprint(author)
